97a8a36b14ff610d26d82d3eb1755697d97e85ea
Genkitahara <gen19990401@gmail.com>
2018-07-12 20:38:02 +0900

    v0.00.6

---
+++ Das Dritte Reich/Assets/Scripts/StartControl.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class StartControl : MonoBehaviour {
+
+    // Use this for initialization
+    public string nextSceneName;
+
+    void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (Time.timeSinceLevelLoad > 5.0f)
+        {
+            SceneManager.LoadScene(nextSceneName, LoadSceneMode.Single);
+        }
+    }
+}
