2bde02f92d9f7c20af8085c595fd987967a35166
北原元輝 <kitaharagenki@kitaharamotokinoMacBook-puro.local>
2018-07-31 15:03:58 +0900

    v0.00.8

---
+++ Das Dritte Reich/Assets/Scripts/CampaignSelectControl.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using TouchScript.Gestures;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class CampaignSelectControl : MonoBehaviour {
+    
+    public TapGesture TheatertapGesture;
+    public TapGesture WarfaretapGesture;
+    public Transform TheaterDiscribePanel;
+    public string nextSceneName;
+
+    private int val = 0;
+	// Use this for ißnitialization
+
+    private void OnEnable()
+    {
+        TheatertapGesture.Tapped += TheaterOnTapped;
+        WarfaretapGesture.Tapped += WarfareOnTapped;
+    }
+
+    private void OnDisable()
+    {
+        TheatertapGesture.Tapped -= TheaterOnTapped;
+        WarfaretapGesture.Tapped -= WarfareOnTapped;
+    }
+
+    private void TheaterOnTapped(object sender, EventArgs e)
+    {
+        TheaterDiscribePanel.transform.position = new Vector3((Screen.width) / 2, (Screen.height) / 2, 0);
+        val = 1;
+    }
+    private void WarfareOnTapped(object sender, EventArgs e)
+    {
+        TheaterDiscribePanel.transform.position = new Vector3((Screen.width) / 2, (Screen.height) / 2, 0);
+        val = 2;
+    }
+    public void SceneLoadStart()
+    {
+        if (val == 1) SceneManager.LoadScene(nextSceneName, LoadSceneMode.Single);
+    }
+}
---
+++ Das Dritte Reich/Assets/Scripts/TouchTest.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using TouchScript.Gestures;
+using UnityEngine;
+
+public class TouchTest : MonoBehaviour
+{
+    public TapGesture tapGesture;
+
+    private void OnEnable()
+    {
+        tapGesture.Tapped += OnTapped;
+    }
+
+    private void OnDisable()
+    {
+        tapGesture.Tapped -= OnTapped;
+    }
+
+    private void OnTapped(object sender, EventArgs e)
+    {
+        Debug.Log("タップされた");
+    }
+}
