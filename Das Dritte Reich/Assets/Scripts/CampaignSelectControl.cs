6c99ec8bdd08579a8cfd35d9742abb61ef31ef8e
北原元輝 <kitaharagenki@kitaharamotokinoMacBook-puro.local>
2018-08-20 21:40:50 +0900

    unko

--- Das Dritte Reich/Assets/Scripts/CampaignSelectControl.cs
+++ Das Dritte Reich/Assets/Scripts/CampaignSelectControl.cs
@@ -8,36 +8,56 @@ public class CampaignSelectControl : MonoBehaviour {
     
     public TapGesture TheatertapGesture;
     public TapGesture WarfaretapGesture;
+    public TapGesture SceneLoadtapGesture;
+    public TapGesture CanceltapGesture;
     public Transform TheaterDiscribePanel;
+    //public TapGesture backScenetapGesture;
     public string nextSceneName;
+    public string previosSceneName;
 
     private int val = 0;
-	// Use this for ißnitialization
+	// Use this for initialization
 
     private void OnEnable()
     {
         TheatertapGesture.Tapped += TheaterOnTapped;
         WarfaretapGesture.Tapped += WarfareOnTapped;
+        SceneLoadtapGesture.Tapped += SceneLoadOnTapped;
+        CanceltapGesture.Tapped += CancelLoadOnTapped;
+        //backScenetapGesture.Tapped += backOnTapped;
     }
 
     private void OnDisable()
     {
         TheatertapGesture.Tapped -= TheaterOnTapped;
         WarfaretapGesture.Tapped -= WarfareOnTapped;
+        SceneLoadtapGesture.Tapped -= SceneLoadOnTapped;
+        CanceltapGesture.Tapped -= CancelLoadOnTapped;
+        //backScenetapGesture.Tapped -= backOnTapped;
     }
 
     private void TheaterOnTapped(object sender, EventArgs e)
     {
-        TheaterDiscribePanel.transform.position = new Vector3((Screen.width) / 2, (Screen.height) / 2, 0);
+        //TheaterDiscribePanel.transform.position = new Vector3((Screen.width) / 2, (Screen.height) / 2, 0);
+        TheaterDiscribePanel.gameObject.SetActive(true);
         val = 1;
     }
     private void WarfareOnTapped(object sender, EventArgs e)
     {
-        TheaterDiscribePanel.transform.position = new Vector3((Screen.width) / 2, (Screen.height) / 2, 0);
+        //TheaterDiscribePanel.transform.position = new Vector3((Screen.width) / 2, (Screen.height) / 2, 0);
+        TheaterDiscribePanel.gameObject.SetActive(true);
         val = 2;
     }
-    public void SceneLoadStart()
+    private void SceneLoadOnTapped(object sender, EventArgs e)
     {
         if (val == 1) SceneManager.LoadScene(nextSceneName, LoadSceneMode.Single);
     }
+    private void CancelLoadOnTapped(object sender, EventArgs e)
+    {
+        TheaterDiscribePanel.gameObject.SetActive(false);
+    }
+    /*private void backOnTapped(object sender, EventArgs e)
+    {
+        SceneManager.LoadScene(previosSceneName, LoadSceneMode.Single);
+    }*/
 }
