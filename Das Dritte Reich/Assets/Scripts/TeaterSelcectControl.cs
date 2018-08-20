54d55e0c7f816676c657d27352d3317eec8a0239
北原元輝 <kitaharagenki@kitaharamotokinoMacBook-puro.local>
2018-08-20 22:09:17 +0900

    unti

---
+++ Das Dritte Reich/Assets/Scripts/TeaterSelcectControl.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using TouchScript.Gestures;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class TeaterSelcectControl : MonoBehaviour {
+
+    public TapGesture PolishtapGesture;
+    public TapGesture WestenFlonttapGesture;
+    public TapGesture EastentapGesture;
+    public TapGesture SceneLoadtapGesture;
+    public TapGesture CancelLoadtapGesture;
+    public TapGesture backScenetapGesture;
+    public Transform DiscribePanel;
+    public Text CampaignSampleText;
+    public string nextSceneName;
+    public string previosSceneName;
+    public string SelectedTheaterName;
+
+    private void OnEnable()
+    {
+        PolishtapGesture.Tapped += PolishOnTapped;
+        WestenFlonttapGesture.Tapped += WestenFlontOnTapped;
+        EastentapGesture.Tapped += EastenOnTapped;
+        SceneLoadtapGesture.Tapped += SceneLoadOnTapped;
+        CancelLoadtapGesture.Tapped += CancelLoadOnTapped;
+        backScenetapGesture.Tapped += backOnTapped;
+    }
+
+    private void OnDisable()
+    {
+        PolishtapGesture.Tapped -= PolishOnTapped;
+        WestenFlonttapGesture.Tapped -= WestenFlontOnTapped;
+        EastentapGesture.Tapped -= EastenOnTapped;
+        SceneLoadtapGesture.Tapped += SceneLoadOnTapped;
+        CancelLoadtapGesture.Tapped += CancelLoadOnTapped;
+        backScenetapGesture.Tapped += backOnTapped;
+    }
+
+    private void PolishOnTapped(object sender, EventArgs e)
+    {
+        DiscribePanel.gameObject.SetActive(true);
+        CampaignSampleText.text = "Polish Campaign";
+        SelectedTheaterName = "PolishCampaign";
+    }
+    private void WestenFlontOnTapped(object sender, EventArgs e)
+    {
+        DiscribePanel.gameObject.SetActive(true);
+        CampaignSampleText.text = "Weasten Flont 1940";
+        SelectedTheaterName = "WeastenFlont1940";
+    }
+    private void EastenOnTapped(object sender, EventArgs e)
+    {
+        DiscribePanel.gameObject.SetActive(true);
+        CampaignSampleText.text = "Easten Front";
+        SelectedTheaterName = "EastenFlont";
+    }
+    private void SceneLoadOnTapped(object sender, EventArgs e)
+    {
+        SceneManager.LoadScene(nextSceneName, LoadSceneMode.Single);
+    }
+    private void CancelLoadOnTapped(object sender, EventArgs e)
+    {
+        DiscribePanel.gameObject.SetActive(false);
+    }
+    private void backOnTapped(object sender, EventArgs e)
+    {
+        SceneManager.LoadScene(previosSceneName, LoadSceneMode.Single);
+    }
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
---
+++ Das Dritte Reich/Assets/Scripts/TheaterMainSceneControl.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using TouchScript.Gestures;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class TheaterMainSceneControl : MonoBehaviour
+{
+
+    public TapGesture backScenetapGesture;
+    public TapGesture WeaponProductiontap;
+    public TapGesture StrategyMissiontap;
+    public TapGesture TechnologyReserchtap;
+
+    public string previosSceneName;
+
+    private void OnEnable()
+    {
+        backScenetapGesture.Tapped += backOnTapped;
+        WeaponProductiontap.Tapped += WeaponProductionOnTapped;
+        StrategyMissiontap.Tapped += StrategyMissionOnTapped;
+        TechnologyReserchtap.Tapped += TechnologyReserchOnTapped;
+    }
+    private void OnDisable()
+    {
+        backScenetapGesture.Tapped -= backOnTapped;
+        WeaponProductiontap.Tapped -= WeaponProductionOnTapped;
+        StrategyMissiontap.Tapped -= StrategyMissionOnTapped;
+        TechnologyReserchtap.Tapped -= TechnologyReserchOnTapped;
+    }
+    void Start()
+    {
+
+    }
+    void Update()
+    {
+
+    }
+    private void WeaponProductionOnTapped(object sender, EventArgs e)
+    {
+        
+    }
+    private void StrategyMissionOnTapped(object sender, EventArgs e)
+    {
+        
+    }
+    private void TechnologyReserchOnTapped(object sender, EventArgs e)
+    {
+        
+    }
+    private void backOnTapped(object sender, EventArgs e)
+    {
+        SceneManager.LoadScene(previosSceneName, LoadSceneMode.Single);
+    }
+}
